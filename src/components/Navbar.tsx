daf16ba83aa848b9668f79531c15a1f315536285
xp-xg <mnzukii@gmail.com>
2025-11-29 11:58:23 +0300

    feat: further increase navbar logo size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,7 +62,7 @@ const Navbar = () => {
                 <img
                   src="/Global kenya containers logo.png"
                   alt="Global Kenya Containers Logo"
-                  className="w-24 h-24 object-contain"
+                  className="w-32 h-32 object-contain"
                 />
               </Link>
               <div>
